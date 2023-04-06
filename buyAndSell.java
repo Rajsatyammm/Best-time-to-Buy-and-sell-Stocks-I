3ae0b7511371ab05ef78128671405d93093b79ed
Satyam Raj <rajsatyammm@gmail.com>
2023-04-07 00:15:45 +0530

    Update buyAndSell.java

--- buyAndSell.java
+++ buyAndSell.java
@@ -1,17 +1,23 @@
-class Solution {
-    public int maxProfit(int[] prices) {
+public class buyAndSell {
+    public static int BuySellStock(int[] Stocks) {
         int buyPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
-
-        for(int i=0; i<prices.length; i++) {
-            if(buyPrice < prices[i]) {
-                int profit = prices[i] - buyPrice;
-                buyPrice = Math.min(buyPrice, prices[i]);
-                maxProfit = Math.max(profit, maxProfit);
-            } else {
-                buyPrice = prices[i];
+        for (int i = 0; i < Stocks.length; i++) {
+            // min = Math.min(min, A[i]);
+            if(buyPrice < Stocks[i]) {
+                int profit = Stocks[i] - buyPrice;
+                buyPrice = Math.min(buyPrice, Stocks[i]);
+                maxProfit = Math.max(maxProfit, profit);
+            } 
+            else {
+                buyPrice = Stocks[i];
             }
         }
         return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] Stocks = {7, 1, 5, 3, 6, 4};
+        System.out.println(BuySellStock(Stocks));
     }
 }
